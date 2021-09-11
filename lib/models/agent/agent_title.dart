f9b63ec68c2b350bab9f7dd768a02bb7960f3561
JoniJohn <whosekid@gmail.com>
2021-09-11 14:41:39 +0200

    Title model classes

--- lib/models/agent/agent_title.dart
+++ lib/models/agent/agent_title.dart
@@ -1,18 +1,20 @@
-class AgentRole {
+class AgentTitle {
   int? id;
-  int? agent; // foreign key
-  int? role; // foreign to role
+  int? agent;
+  int? dept;
+  int? title;
   String? start; // start date
   String? end; // end date
-  String? reason; // end reason, retired? promotion? termination? resign?
+  String? reason;
 
-  AgentRole(this.agent, this.role, this.start);
+  AgentTitle(this.agent, this.dept, this.title, this.start);
 
   Map<String, dynamic> toMap() {
     var map = <String, dynamic>{};
 
     map['agent_id'] = agent;
-    map['role_id'] = role;
+    map['dept_id'] = dept;
+    map['title_id'] = title;
     map['start_date'] = start;
     map['end_date'] = end;
     map['reason'] = reason;
@@ -21,10 +23,11 @@ class AgentRole {
     return map;
   }
 
-  AgentRole.fromObject(dynamic obj) {
+  AgentTitle.fromObject(dynamic obj) {
     id = obj['id'];
-    role = obj['role_id'];
     agent = obj['agent_id'];
+    dept = obj['dept_id'];
+    title = obj['title_id'];
     start = obj['start_date'];
     end = obj['end_date'];
     reason = obj['reason'];
--- lib/models/agent/title.dart
+++ lib/models/agent/title.dart
@@ -1,26 +1,23 @@
-class Role {
+class JobTitle {
   int? id;
-  String? name; // options Support
+  String? name; // e.g Manager
   String? desc;
-  int? dept; // foreign key to department
 
-  Role(this.name, this.desc);
+  JobTitle(this.name, this.desc);
 
   Map<String, dynamic> toMap() {
     var map = <String, dynamic>{};
 
     map['name'] = name;
     map['description'] = desc;
-    map['dept_id'] = dept;
     if (id != null) map['id'] = id;
 
     return map;
   }
 
-  Role.fromObject(dynamic obj) {
+  JobTitle.fromObject(dynamic obj) {
     id = obj['id'];
     name = obj['name'];
     desc = obj['description'];
-    dept = obj['dept_id'];
   }
 }
